51655800ae9b35ac81d4575c80d6f1fe00db5c5d
Saiyed-Faroooq <147801152+Saiyed-Faroooq@users.noreply.github.com>
2023-11-03 17:31:08 +0530

    Delete ecommerce directory

--- ecommerce/ecommerceproject/search_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .import views
-from django.conf import settings
-
-app_name = 'search_app'
-
-urlpatterns = [
-    path('', views.searchresult, name='searchresult'),
-]
